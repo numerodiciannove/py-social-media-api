30b34b51ed9e37a3b270b4c34cbd1aa12bccb424
numerodiciannove <5079447@gmail.com>
2024-04-13 21:19:37 +0300

    Fixed post_image_file_path func

--- post/models.py
+++ post/models.py
@@ -9,7 +9,7 @@ from django.utils.translation import gettext as _
 
 def post_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/posts/", filename)
 
@@ -25,18 +25,14 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(
-        _("post_image"),
-        null=True,
-        upload_to=post_image_file_path
+        _("post_image"), null=True, upload_to=post_image_file_path
     )
     author = models.ForeignKey(
         get_user_model(), related_name="autor_posts", on_delete=models.CASCADE
     )
     tags = models.ManyToManyField(Tag, related_name="post_hashtags")
     likes = models.ManyToManyField(
-        get_user_model(),
-        related_name="liked_posts",
-        blank=True
+        get_user_model(), related_name="liked_posts", blank=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
     changed_at = models.DateTimeField(auto_now=True)
@@ -49,8 +45,7 @@ class Post(models.Model):
 
     def __str__(self):
         return (
-            f"{self.author.username}'s post "
-            f"at {self.created_at}. Tags {self.tags}"
+            f"{self.author.username}'s post " f"at {self.created_at}. Tags {self.tags}"
         )
 
 
@@ -68,6 +63,5 @@ class Comment(models.Model):
 
     def __str__(self):
         return (
-            f"Comment by {self.author.username} "
-            f"on {self.post} at {self.created_at}"
+            f"Comment by {self.author.username} " f"on {self.post} at {self.created_at}"
         )
