f1c301b292ef5fa0581e688f137f1562554a6a2a
numer <5079447@gmail.com>
2024-04-16 21:35:09 +0300

    Added comments endpoint from CommentViewSet.

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from post.views import TagViewSet, PostViewSet
+from post.views import TagViewSet, PostViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 router.register("tags", TagViewSet)
 router.register("posts", PostViewSet)
+router.register("comments", CommentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
